2571484949be3558141ad9138343b11468e15d01
John <mjohncorbet@gmail.com>
2022-09-20 12:29:26 +0800

    created-singleRound-function

--- scriptName.js
+++ scriptName.js
@@ -2,27 +2,31 @@
 const array = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    const compRandom = Math.floor(Math.random() * Array.length);
+    const compRandom = Math.floor(Math.random() * array.length);
     const compChoice = array[compRandom];
     return compChoice; 
 }
 
 function playRound(playerSelection, computerSelection) {
     let message;
-    if(
+    if (
         (playerSelection === "Rock" && computerSelection === "Scissors")
 
         || (playerSelection === "Paper" && computerSelection === "Rock") 
 
         || (playerSelection === "Scissors" && computerSelection === "Paper")
-    ) {
-        message = `You Win! ${playerSelection} beats ${computerSelection}`;
-   
-    }
+
+    ) { message = `You Win! ${playerSelection} beats ${computerSelection}`;
+
+    } else if (playerSelection === computerSelection) {
+        message = "Draw!";
+    } else {
+        message = `You Lose! ${computerSelection} beats ${playerSelection}`};
+
     return message;
 }
 
-const computerSelection = "Rock";
-const playerSelection = "Paper";
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
 
 console.log(playRound(playerSelection, computerSelection));
